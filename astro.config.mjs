99705ec9007d7908e908f51707f94d2e2253bc82
Javiani <edudu_tata@hotmail.com>
2022-11-20 23:35:16 -0300

    Adding critical path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
 import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
+import critters from 'astro-critters'
+
 import rupture from 'rupture'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [compress()],
-	vite: {
-		ssr: {
-			external: ['svgo']
-		},
-		css: {
-			preprocessorOptions: {
-				styl: {
-					use: rupture(),
-					paths:['src/']
-				}
-			}
-		}
-	  }
+  integrations: [compress(), critters()],
+  vite: {
+    ssr: {
+      external: ['svgo']
+    },
+    css: {
+      preprocessorOptions: {
+        styl: {
+          use: rupture(),
+          paths: ['src/']
+        }
+      }
+    }
+  }
 })
