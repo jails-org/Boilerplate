d02b371f07c0a58ed2e1751919bb2f0070ed05f6
Javiani <edudu_tata@hotmail.com>
2023-04-11 22:22:10 -0300

    Tailwind first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,16 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import critters from 'astro-critters'
-import rupture from 'rupture'
+import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import critters from 'astro-critters';
+import rupture from 'rupture';
+
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), critters()],
-  vite: {
-    ssr: {
-      external: ['svgo']
-    },
-    css: {
-      preprocessorOptions: {
-        styl: {
-          use: rupture(),
-          paths: ['src', 'node_modules'],
-		  resolveURL : true,
-		  includeCSS : true,
-		  additionalData: `
-		  	@require 'ui-system/variables'
-		  `
-        }
-      }
-    }
-  }
+	integrations: [compress(), critters(), tailwind()],
+	vite: {
+		ssr: {
+			external: ['svgo']
+		}
+	}
 })
