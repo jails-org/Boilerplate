f1404494e3920fdaadf29f99850780f0dea76e4f
Javiani <edudu_tata@hotmail.com>
2023-07-02 19:33:25 -0300

    Changing jest to vitest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,28 @@
-import { defineConfig } from 'astro/config';
-import critters from 'astro-critters';
-import tailwind from '@astrojs/tailwind';
-import image from "@astrojs/image";
-
-import compress from "astro-compress";
+import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
+import critters from 'astro-critters'
+import tailwind from '@astrojs/tailwind'
+import image from "@astrojs/image"
+import rupture from 'rupture'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [critters(), tailwind(), image(), compress()],
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
-  }
-});
+	integrations: [compress(), critters(), image(), tailwind()],
+	vite: {
+		ssr: {
+			external: ['svgo']
+		},
+		css: {
+			preprocessorOptions: {
+				styl: {
+					use: rupture(),
+					paths: ['src', 'node_modules'],
+					resolveURL : true,
+					includeCSS : true,
+					additionalData: ``
+				}
+			}
+		}
+	}
+})
+
--- src/pages/home/__tests__/index.spec.ts
+++ src/pages/home/__tests__/index.spec.ts
@@ -1,19 +1,19 @@
-
+import { describe, test, expect } from 'vitest'
 import { store } from '../_use-cases/store'
 import { getUsers } from '../_services/users'
 
 describe('THE USERS LIST', () => {
 
-	it('Should add a users list into the store', async () => {
+	test('Should add a users list into the store', async () => {
 		const newusers = await getUsers()
-		store.dispatch('USERS_LIST_SET', { users: newusers })
+		await store.dispatch('USERS_LIST_SET', { users: newusers })
 		const { users } = store.getState()
 		expect( users ).toEqual( newusers )
 	})
 
-	it('Should be able to remove a user from the list by id', async () => {
+	test('Should be able to remove a user from the list by id', async () => {
 		const newusers = await getUsers()
-		store.dispatch('USERS_LIST_REMOVE', { id : 1 })
+		await store.dispatch('USERS_LIST_REMOVE', { id : 1 })
 		const { users } = store.getState()
 		expect( users.length ).toEqual(newusers.length - 1)
 	})
