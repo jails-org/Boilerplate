f665b8c75348d0e43b089fd9272017f461c06a1f
Javiani <eduardo.ottaviani@gmail.com>
2024-11-19 23:46:18 -0300

    Upgrading Boilerplate

--- src/pages/home/_services/users/index.ts
+++ src/pages/home/_services/users/index.ts
@@ -1,10 +1,7 @@
-import axios from 'axios'
 import { User } from '../../_entities/user'
 
-const baseURL = 'https://jsonplaceholder.typicode.com'
-const http = axios.create({ baseURL })
-
 export const getUsers = async () => {
-	const { data:users } = await http.get('/users')
-	return users.map( User )
+	return fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then( users => users.map(User) )
 }
