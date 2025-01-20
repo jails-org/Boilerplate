80ca55a6933f1db8c5dbdc21c7dc1713f85988f0
Javiani <eduardo.ottaviani@gmail.com>
2025-01-19 23:11:31 -0300

    Updating test case with jails.std/store

--- src/pages/home/__tests__/index.spec.ts
+++ src/pages/home/__tests__/index.spec.ts
@@ -1,20 +1,30 @@
 import { describe, test, expect } from 'vitest'
-import * as usersCase from '../_use-cases/users'
-import { getUsers } from '../_services/users'
+import { usersCase } from '../_use-cases/users'
 
 describe('THE USERS LIST', () => {
 
+	test('Should loading state be true when fetching users', async () => {
+		usersCase.dispatch('FETCH_USERS')
+			.then((state) => expect( state.loading ).toEqual( true ))
+	})
+
 	test('Should add a users list into the store', async () => {
-		const newusers = await getUsers()
-		usersCase.setUsers( newusers )
-		const users = usersCase.users.value
-		expect( users ).toEqual( newusers )
+
+		const state = await new Promise((resolve) => {
+			usersCase.dispatch('FETCH_USERS')
+			usersCase.patternMatch({
+				SET_USERS( state ) {
+					resolve(state)
+				}
+			})
+		})
+
+		expect( state.users.length ).toEqual( 10 )
 	})
 
 	test('Should be able to remove a user from the list by id', async () => {
-		const newusers = await getUsers()
-		usersCase.removeUser( 1 )
-		const users = usersCase.users.value
-		expect( users.length ).toEqual(newusers.length - 1)
+		const state = await usersCase.dispatch('REMOVE_USER', { id : '1' })
+		expect( state.users.length ).toEqual( 9 )
 	})
+
 })
--- src/pages/home/_use-cases/users.ts
+++ src/pages/home/_use-cases/users.ts
@@ -1,13 +1,31 @@
-import { signal } from 'jails.pandora/store'
+import { Store } 	from 'jails.std/store'
+import { getUsers } from '../_services/users'
 
-export { effect } from 'jails.pandora/store'
-
-export const users = signal([])
-
-export const setUsers = ( list ) => {
-	users.value = list
+const initialState = {
+	users	: [],
+	loading	: false
 }
 
-export const removeUser = (id) => {
-	users.value = users.value.filter( item => item.id !== id )
+const actions = {
+
+	FETCH_USERS( state, payload, { dispatch }) {
+		getUsers().then( users => dispatch('SET_USERS', { users }) )
+		return {
+			loading: true
+		}
+	},
+
+	SET_USERS( state, { users }) {
+		return {
+			users
+		}
+	},
+
+	REMOVE_USER( state, { id }) {
+		return {
+			users: state.users.filter( user => user.id !== id )
+		}
+	}
 }
+
+export const usersCase = Store( initialState, actions )
