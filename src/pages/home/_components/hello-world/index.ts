31fa95d42d5a3b093e3617ad94ba09eb5de18da4
Javiani <eduardo.ottaviani@gmail.com>
2025-02-23 23:41:05 -0300

    Fixing bug

--- src/pages/home/_components/hello-world/index.ts
+++ src/pages/home/_components/hello-world/index.ts
@@ -4,38 +4,37 @@ import { animation } from './utils/animation'
 
 export default async function helloWorld ({ main, elm, on, state }) {
 
-    const wrapper = elm.querySelector('.swiper')
-    const canvas = elm.querySelector('canvas')
-    const { checking, numLook, handsUp, trigSuccess } = await animation( canvas )
-    const swiper = new Swiper(wrapper, { allowTouchMove: false })
+	const wrapper = elm.querySelector('.swiper')
+	const canvas = elm.querySelector('canvas')
+	const { checking, numLook, handsUp, trigSuccess } = await animation( canvas )
+	const swiper = new Swiper(wrapper, { allowTouchMove: false })
 
-    let formData = {}
+	let formData = {}
 
-    main( _ => {
+	main( _ => {
 
-        on('input', 'input[type="text"]', ontextchange)
+		on('input', 'input[type="text"]', ontextchange)
 		on('focus', 'input[type="text"]', ontextfocus)
 		on('focus', 'input[name="password"]', onPasswordFocus)
 		on('blur', 'input[name="password"]', onPasswordBlur)
 		on('click', 'button[data-reset]', onReset)
-        on('form-validation:submit', 'form', submit)
-        on('click', '[data-prev]', prev)
+		on('click', '[data-prev]', prev)
+		on('form-validation:submit', 'form', submit)
 
-        swiper.on('transitionEnd', onslidechange)
+		swiper.on('transitionEnd', onslidechange)
 
-		getUsers()
-			.then( users => state.set({ users }) )
+		getUsers().then( users => state.set({ users }) )
 
-    })
+	})
 
-    const ontextchange = (e) => {
+	const ontextchange = (e) => {
 		const num = e.target.value.length
 		numLook.value = num * 1.6
 	}
 
-    const ontextfocus = (e) => {
-        checking.value = true
-    }
+	const ontextfocus = (e) => {
+		checking.value = true
+	}
 
 	const onPasswordFocus = (e) => {
 		handsUp.value = true
@@ -49,38 +48,37 @@ export default async function helloWorld ({ main, elm, on, state }) {
 		checking.value = false
 	}
 
-    const onslidechange = (swiper) => {
-        const index = swiper.activeIndex
-        const current = swiper.slides[index]
-        const input = current.querySelector('input,select,textarea')
-        input.focus()
-    }
+	const onslidechange = (swiper) => {
+		const index = swiper.activeIndex
+		const current = swiper.slides[index]
+		const input = current.querySelector('input,select,textarea')
+		input.focus()
+	}
 
-    const submit = (e, { data }) => {
-        formData = {...formData, ...data }
+	const submit = (e, { data }) => {
+		console.log( e, data, formData )
+		formData = {...formData, ...data }
+		if( e.delegateTarget.finish ) {
+			send()
+		} else {
+			swiper.slideNext()
+		}
+	}
 
-        if( e.delegateTarget.finish ) {
-            send()
-        } else {
-            swiper.slideNext()
-        }
-    }
+	const prev = () => {
+		swiper.slidePrev()
+		state.set({ success: null })
+	}
 
-    const prev = () => {
-        swiper.slidePrev()
-        state.set({ success: null })
-    }
-
-    const send = () => {
-        state.set({ success: true, formData })
-            .then( _ => elm.querySelectorAll('form') )
-            .then( forms => forms.forEach( form => form.reset()))
-        setTimeout( _ => trigSuccess.fire(), 2000 )
-    }
+	const send = () => {
+		state.set({ success: true, formData })
+			.then( _ => elm.querySelectorAll('form') )
+		setTimeout( _ => trigSuccess.fire(), 2000 )
+	}
 }
 
 export const model = {
 	users: null,
-    success: null,
-    formData: null
+	success: null,
+	formData: null
 }
